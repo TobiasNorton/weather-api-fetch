522b89d17f529b2332315106e5483117bec6608c
Tobias Norton <tnorton20@yahoo.com>
2018-11-06 16:03:08 -0500

    Separated into respective DOM and API classes

--- public/main.js
+++ public/main.js
@@ -1,68 +1,90 @@
 const searchByCity = () => {
-  // When the user clicks on the search button:
-  // - Get the city
-  //   - Find the DOM element of the input field
-  let inputField = document.querySelector('.selected-city')
-  //   - Read the user input from that field
-  //   - Save that in a variable called "selectedCity"
-  let selectedCity = inputField.value
-  //   - Build a URL with the "selectedCity" and API key
-  let cityURL = `https://api.openweathermap.org/data/2.5/weather?q=${selectedCity}&APPID=02b2a9ead3ec838a723d7f65a90a6b6e&units=imperial`
-  fetch(cityURL) // <-----this is the promise, let promise = fetch(cityURL)
-    .then(response => response.json()) // <----The response is what the promise returns, which we covert to json
-    .then(weatherData => {
-      console.log(`Showing weather for ${selectedCity}`)
-      // Do whatever we want under this
-      let weatherList = document.querySelector('.weather-list')
-
-      let temperature = document.createElement('p')
-      temperature.textContent = `Temperature: ${weatherData.main.temp}˚F`
-      weatherList.appendChild(temperature)
-
-      let hiTemp = document.createElement('p')
-      hiTemp.textContent = `High: ${weatherData.main.temp_max}˚F`
-      weatherList.appendChild(hiTemp)
-
-      let lowTemp = document.createElement('p')
-      lowTemp.textContent = `Low: ${weatherData.main.temp_min}˚F`
-      weatherList.appendChild(lowTemp)
-
-      let humidity = document.createElement('p')
-      humidity.textContent = `Humidity: ${weatherData.main.humidity}`
-      weatherList.appendChild(humidity)
-    })
+  // Get the value of the user input, which takes place in the DOM
+  let domUpdater = new DOMUpdater()
+  let city = domUpdater.getUserInputForCity()
+  // Use the user's click to fetch the data for that city input
+  let weatherAPI = new WeatherAPI()
+  weatherAPI.fetchWeatherForCity(city)
+  // Display the forecast to the user (in the DOM)
+  let weatherDisplay = new DOMUpdater()
+  weatherDisplay.addForecastToDOM()
 }
 
 const searchByZip = () => {
-  let inputField = document.querySelector('.selected-zip')
-  let selectedZip = inputField.value
-  let zipURL = `https://api.openweathermap.org/data/2.5/weather?zip=${selectedZip}&APPID=02b2a9ead3ec838a723d7f65a90a6b6e&units=imperial`
+  let domUpdater = new DOMUpdater()
+  let zip = domUpdater.getUserInputForZip()
+  let weatherAPI = new WeatherAPI()
+  weatherAPI.fetchWeatherForCity(zip)
+}
 
-  fetch(zipURL)
-    .then(response => response.json())
-    .then(weatherData => {
-      console.log(`Showing weather for ${selectedZip}`)
-      let weatherList = document.querySelector('.weather-list')
+class DOMUpdater {
+  constructor() {}
 
-      let temperature = document.createElement('p')
-      temperature.textContent = `Temperature: ${weatherData.main.temp}˚F`
-      weatherList.appendChild(temperature)
+  getUserInputForCity() {
+    let inputField = document.querySelector('.selected-city')
+    return inputField.value
+  }
 
-      let hiTemp = document.createElement('p')
-      hiTemp.textContent = `High: ${weatherData.main.temp_max}˚F`
-      weatherList.appendChild(hiTemp)
+  getUserInputForZip() {
+    let inputField = document.querySelector('.selected-zip')
+    return inputField.value
+  }
 
-      let lowTemp = document.createElement('p')
-      lowTemp.textContent = `Low: ${weatherData.main.temp_min}˚F`
-      weatherList.appendChild(lowTemp)
+  addForecastToDOM(temp, high, low, hum) {
+    let weatherList = document.querySelector('.weather-list')
+    let temperature = document.createElement('p')
+    temperature.textContent = `Temperature: ${temp}˚F`
+    weatherList.appendChild(temperature)
 
-      let humidity = document.createElement('p')
-      humidity.textContent = `Humidity: ${weatherData.main.humidity}`
-      weatherList.appendChild(humidity)
+    let hiTemp = document.createElement('p')
+    hiTemp.textContent = `High: ${high}˚F`
+    weatherList.appendChild(hiTemp)
 
-      console.log(weatherData.main.temp)
-      console.log(weatherData)
-    })
+    let lowTemp = document.createElement('p')
+    lowTemp.textContent = `Low: ${low}˚F`
+    weatherList.appendChild(lowTemp)
+
+    let humidity = document.createElement('p')
+    humidity.textContent = `Humidity: ${hum}`
+    weatherList.appendChild(humidity)
+  }
+}
+
+class WeatherAPI {
+  constructor() {}
+
+  fetchWeatherForCity(selectedCity) {
+    let cityURL = `https://api.openweathermap.org/data/2.5/weather?q=${selectedCity}&APPID=02b2a9ead3ec838a723d7f65a90a6b6e&units=imperial`
+
+    fetch(cityURL) // <-----this is the promise, let promise = fetch(cityURL)
+      .then(response => response.json()) // <----The response is what the promise returns,
+      .then(weatherData => {
+        // get specific elements
+        let temperature = weatherData.main.temp
+        let hiTemp = weatherData.main.temp_max
+        let lowTemp = weatherData.main.temp_min
+        let humidity = weatherData.main.humidity
+
+        let domUpdater = new DOMUpdater()
+        domUpdater.addForecastToDOM(temperature, hiTemp, lowTemp, humidity)
+      })
+  }
+
+  fetchWeatherForZip(selectedZip) {
+    let zipURL = `https://api.openweathermap.org/data/2.5/weather?q=${selectedZip}&APPID=02b2a9ead3ec838a723d7f65a90a6b6e&units=imperial`
+
+    fetch(zipURL) // <-----this is the promise, let promise = fetch(cityURL)
+      .then(response => response.json()) // <----The response is what the promise returns,
+      .then(weatherData => {
+        let temperature = weatherData.main.temp
+        let hiTemp = weatherData.main.temp_max
+        let lowTemp = weatherData.main.temp_min
+        let humidity = weatherData.main.humidity
+
+        let domUpdater = new DOMUpdater()
+        domUpdater.addForecastToDOM(temperature, hiTemp, lowTemp, humidity)
+      })
+  }
 }
 
 const main = () => {
